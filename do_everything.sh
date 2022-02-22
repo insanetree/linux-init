8b61d8ce435b8666530fc22506a627e19d57ffb3
insanetree <p0p0v1c.41ek54nd4r@gmail.com>
2022-02-22 22:22:03 +0100

    clone and install dwm from github

--- do_everything.sh
+++ do_everything.sh
@@ -5,3 +5,7 @@ sh ./packages/install.sh
 
 #config dotfiles
 sh ./dotfiles/config_dotfiles.sh
+
+#installing dwm and addons
+git clone https://github.com/insanetree/dwm
+make clean install -C ./dwm/
