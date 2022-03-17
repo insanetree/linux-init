7dd574f7e2aa7a77be31a9dbd666c3ee7bace8ed
insanetree <p0p0v1c.41ek54nd4r@gmail.com>
2022-03-17 01:36:02 +0100

    installing slstatus too

--- do_everything.sh
+++ do_everything.sh
@@ -6,10 +6,12 @@ sh ./packages/install.sh
 #config dotfiles
 sh ./dotfiles/config_dotfiles.sh
 
-#installing dwm and addons
+#installing suckless stuff
 git clone https://github.com/insanetree/dwm
 doas make clean install -C ./dwm/
 git clone https://github.com/insanetree/dmenu
 doas make clean install -C ./dmenu/
 git clone https://github.com/insanetree/st
 doas make clean install -C ./st/
+git clone https://github.com/insanetree/slstatus
+doas make clean inslstatusall -C ./slstatus/
